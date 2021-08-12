452ed9f31769e2ab8710d395f8a48153ac22d95b
WillWu <williamwu8-8@outlook.com>
2021-08-12 18:00:52 -0500

    Updated Governing Differential Equations

--- TSPlant.py
+++ TSPlant.py
@@ -40,11 +40,15 @@ class Tailsitter:
         # elements: [prop_v_l, prop_v_r, flap_deg_l, flap_deg_r]'
         self.control = sym.zeros(4,1)
 
-
     def CalcThrust(self, prop_v):
         # helper function for aero calculation
         # calculate thrust given propeller speed
         return self.c_thrust * self.rho * self.prop_d ** 4 * prop_v**2
+
+    def CalcAOA(self):
+        # helper function for aero calculation
+        # return angle of attack at that instant
+        return sym.N(self.state[5]/self.state[3])
 
     def CalcEffCons(self, prop_v):
         # helper function for aero calculation
@@ -108,3 +112,25 @@ class Tailsitter:
         pitch_right = (effect_constant_r * self.c_bar
                        * (self.c_moment + self.c_moment_flap * flap_deg_r))
         return pitch_left - pitch_right
+
+    def RotationMatrix(self, q):
+        # helper function for plant model
+        # generates an earth to body rotation matrix based on current quaternion
+        return sym.Matrix([
+            [q[0]**2 + q[1]**2 - q[2]**2 -q[3]**2, 2*(q[1]*q[2]+q[0]*q[3]),
+             2*(q[1]*q[3]-q[0]*q[2])], # first row
+            [2*(q[1]*q[2]-q[0]*q[3]), q[0]**2 - q[1]**2 + q[2]**2 - q[3]**3,
+             2*(q[2]*q[3] + q[0]*q[1])], # second row
+            [2*(q[1]*q[3] + q[0] * q[2]), 2*(q[2]*q[3] - q[0]*q[1]),
+                q[0]**2 - q[1]**2 -q[2]**2 + q[3]**2]]) # third row
+
+    def VDot(self, omega, v, q, u):
+        # Dynamics Equation: v-dot
+        # takes four state vectors: omega(angular rate, 3x1),
+        # v(translational rate, 3x1), q(body rotation, 4x1), and u(control, 4x1)
+
+        # circular acceleration and gravity
+        acc_grav = (-omega.cross(v)
+                    + self.RotationMatrix(q) * sym.Matrix([[0], [0], [g]]))
+
+        return
