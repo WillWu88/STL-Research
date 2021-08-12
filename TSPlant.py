49abfa7f470ab82d44ccad064405aceee8bee891
WillWu <williamwu8-8@outlook.com>
2021-08-12 12:14:09 -0500

    finished aero helper functions

--- TSPlant.py
+++ TSPlant.py
@@ -44,7 +44,13 @@ class Tailsitter:
     def CalcThrust(self, prop_v):
         # helper function for aero calculation
         # calculate thrust given propeller speed
-        thrust_coeff = self.c_thrust * self.rho * self.prop_d ** 4 * prop_v**2
+        return self.c_thrust * self.rho * self.prop_d ** 4 * prop_v**2
+
+    def CalcEffCons(self, prop_v):
+        # helper function for aero calculation
+        # calculate effect constant: dynamic pressure times wing area of section
+        return (4 * self.CalcThrust(prop_v)
+                / (np.pi * self.prop_d**2) * self.wing_area / 6)
 
     def CalcLift(self, prop_v, flap_deg):
         # helper function for aero calculation
@@ -52,8 +58,7 @@ class Tailsitter:
         # only calculating one side, assuming there can be different prop speed
 
         # dynamic pressure times effective wing area
-        effect_constant = (4 * CalcThrust(prop_v)
-                     / (np.pi * self.prop_d**2) * self.wing_area / 6)
+        effect_constant = self.CalcEffCons(prop_v)
         return (effect_constant * (self.c_lift + self.c_lift_flap * flap_deg))
 
     def CalcDrag(self, prop_v, flap_deg):
@@ -61,20 +66,19 @@ class Tailsitter:
         # calculate aerodynamic drag given prop seped and flap deviation
         # only calculating oneside, assuming there can be different prop speed
         # on both sides
-        effect_constant = (4 * CalcThrust(prop_v)
-                     / (np.pi * self.prop_d**2) * self.wing_area / 6)
+        effect_constant = self.CalcEffCons(prop_v)
         return effect_constant * self.c_drag_flap * np.abs(self.flap_deg)
 
-    def CalcAeroRoll(self, flap_deg_l, flap_deg_r):
+    def CalcAeroRoll(self, prop_v_l, prop_v_r, flap_deg_l, flap_deg_r):
         # helper function for aero calculation
-        # calculate aerodynamic roll given flap deflection on both sides
+        # calculate aerodynamic roll given flap deflection and prop speed
         # postive deflection as downwards
         # postive left deflection results in a positive roll
-        effect_constant = (4 * CalcThrust(prop_v)
-                     / (np.pi * self.prop_d**2) * self.wing_area / 6)
-        roll_left = (effect_constant * self.wing_span
+        effect_constant_l = self.CalcEffCons(prop_v_l)
+        effect_constant_r = self.CalcEffCons(prop_v_r)
+        roll_left = (effect_constant_l * self.wing_span
                      * self.c_roll_flap * flap_deg_l)
-        roll_right = (effect_constant * self.wing_span
+        roll_right = (effect_constant_r * self.wing_span
                       * self.c_roll_flap * flap_deg_r)
         return roll_left - roll_right
 
@@ -91,3 +95,16 @@ class Tailsitter:
         # calculate yaw due to thrust given prop speed
         return (self.prop_arm
                 * (self.CalcThrust(prop_v_l) - self.CalcThrust(prop_v_r)))
+
+    def CalcAeroPitch(self, prop_v_l, prop_v_r, flap_deg_l, flap_deg_r):
+        # helper function for aero calculation
+        # calculate aerodynamic pitch given flap deflection and prop speed
+        # postive deflection as downwards
+        # postive left deflection results in a positive pitch
+        effect_constant_l = self.CalcEffCons(prop_v_l)
+        effect_constant_r = self.CalcEffCons(prop_v_r)
+        pitch_left = (effect_constant_l * self.c_bar
+                      * (self.c_moment + self.c_moment_flap * flap_deg_l))
+        pitch_right = (effect_constant_r * self.c_bar
+                       * (self.c_moment + self.c_moment_flap * flap_deg_r))
+        return pitch_left - pitch_right
